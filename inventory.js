226a033502b1d35443b35bdcfaa40982659bcf78
devjanaprime <dev@primeacademy.io>
2018-02-02 09:44:13 -0600

    basic addition functionality

--- inventory.js
+++ inventory.js
@@ -1,5 +1,38 @@
-console.log( 'js' );
+var inventory = [];
 
 $( document ).ready( function(){
-  console.log( 'JQ' );
-});
+    $( '#addButton' ).on( 'click', function(){
+      console.log( 'in addButton on click' );
+      // get user input
+      var newObject = {
+        size: $( '#sizeIn' ).val(),
+        color: $( '#colorIn' ).val(),
+        description: $( '#descriptionIn' ).val()
+      }; // end newObject
+      console.log( 'adding:', newObject );
+      // push into an array
+      inventory.push( newObject );
+      // update DOM
+      displayInventory();
+      // display a success alert
+      alert( 'Added to inventory!' );
+    }); //end addButton on click
+}); // end doc ready
+
+function displayInventory(){
+  console.log( 'in displayInventory' );
+  // stored the ul elemet in an array
+  var outputList = $( '#inventoryOut' );
+  // empty ul
+  outputList.empty();
+  //loop through inventory array
+  for( var i=0; i<inventory.length; i++ ){
+    // append each item to the ul element
+    var stringToAppend = '<li>';
+    stringToAppend += inventory[ i ].size + ' ';
+    stringToAppend += inventory[ i ].color + ' ';
+    stringToAppend += inventory[ i ].description;
+    stringToAppend += '</li>';
+    outputList.append( stringToAppend );
+  } // end for
+} // end displayInventory
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 Gacrux JQuery/Javascript Synthesis (Do You Have This?)
 ===
 
+"core loop":
+---
 Stub out interface
 Basic scripting - i/o
 Processing
+Output
